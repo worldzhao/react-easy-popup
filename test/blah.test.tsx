a9bbd07d0cf22c5289ec1b67d62d60894b1ee12e
zhiwen.zhao <zhiwen.zhao@ximalaya.com>
2020-05-19 08:35:57 +0800

    chore: use np to publish package

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Popup from '../src';
+import { Popup } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
